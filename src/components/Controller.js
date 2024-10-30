b3066fe2c82dbc5d3217f85992f55ed95039a356
Arnav Jain <jainarnav@outlook.com>
2024-10-30 18:34:05 +0530

    initialized router

---
+++ src/components/Controller.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Controller() {
+    return <div className="cont">
+        <h2>ARNAV JAIN</h2>
+
+        <div className="cont-right">
+            <a href="/projects" className="link">Projects</a>
+            <a href="#" className="link">About Me</a>
+            <a href="#" className="link">Contact</a>
+        </div>
+    </div>;
+}
+
+export default Controller;
